d916a744b5c7700f03232a0b3cd2e2c7f4006247
Naimur00 <aminaim4u@gmail.com>
2024-11-11 15:54:37 +0600

    update: create page

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
-type Props = {}
-
-const page = (props: Props) => {
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  const customers = await fetchCustomers();
+ 
   return (
-    <div>create page</div>
-  )
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
 }
-
-export default page
